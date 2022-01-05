69f04ef484418ad98a25a576ff2f9d3d53a1f4e2
Mateo Navarrette <mateonav24@gmail.com>
2022-01-05 15:36:56 -0800

    contains close num, duplicate, convert celsius

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,12 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
-
+    for(let i=0;i < nums.length;i++){
+        for(let j= i + 1;j < nums.length;j++){
+            if(nums[i] === nums[j]){
+                return Math.abs(i - j) <= k;
+            }
+        }
+    }
 }
 
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,25 @@
 export function containsDuplicates(a: number[]): boolean {
+    //double for loop solution
+    // for(let i=0;i < a.length;i++){
+    //     for(let j= i + 1;j < a.length;j++){
+    //         if(a[i] === a[j]){
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false
 
+    //single for loop solution
+    //more efficient runtime b/c it'll catch lower numbers sooner
+    a = a.sort((a, b) => a - b)
+    
+    for(let i=0;i < a.length;i++){
+        if(a[i] === a[i + 1]){
+            return true
+        }
+    }
+    return false
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));

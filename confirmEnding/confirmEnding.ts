ab1a5a27d4aa8d2d488615a9e3561eda55d6c01f
Mateo Navarrette <mateonav24@gmail.com>
2022-01-05 12:53:33 -0800

    confirm ending

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,13 @@
 export function confirmEnding(str: string, target: string) {
+    //using endsWith()
+    //return str.endsWith(target)
 
+    //using substring
+    //use target.length to get our first index for substring
+    //use the total string length as our second index for substring
+    //compare the substring to the target
+    return str.substring(target.length - 1, str.length) === target
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));

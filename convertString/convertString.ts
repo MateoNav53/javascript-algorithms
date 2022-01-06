1ea255aabac3beccde1a922bdd16d99d7177c218
Mateo Navarrette <mateonav24@gmail.com>
2022-01-06 12:52:01 -0800

    crossingSums and convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,16 @@
 export function convertString(s: string, t: string): boolean {
-
+    let firstWord = s.split('');
+    let secondWord = t.split('');
+    let string = ""
+    let tIndex = 0
+    for(let i=0;i < firstWord.length;i++){
+        if(firstWord[i] === secondWord[tIndex]){
+            string += firstWord[i]
+            tIndex++
+        }
+    }
+    return string === t;
 }
 
-// console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-// console.log(convertString('addbyca', 'abcd'));
+console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
+console.log(convertString('addbyca', 'abcd'));
--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,22 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
+    let row = matrix[a].reduce((a, b) => a + b, 0)
+    let total = 0
+    //using forEach, subtracting value we want to omit at the end
+    // matrix.forEach((arr) => {
+    //     total += arr[b]
+    // })
+    // return (total - matrix[a][b]) + row
+
+    //for loop
+    //if our index is the value we want to omit, add 0
+    //otherwise, add the value in row a, index b
+    for(let i=0;i < matrix.length;i++){
+        total += i === a ? 0 : matrix[i][b];
+    }
+    return row + total
 
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1], 
+    [2, 2, 2, 2], 
+    [3, 3, 3, 3]], 1, 3));

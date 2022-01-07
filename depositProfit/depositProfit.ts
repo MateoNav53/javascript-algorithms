363f3734be30a5f6cd3e1c12fff479df53f8ef79
Mateo Navarrette <mateonav24@gmail.com>
2022-01-06 18:17:56 -0800

    deposit profit and different symbol

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,14 @@
 export function depositProfit(deposit: number, rate: number, threshold: number): number {
-
+    let total = 0;
+    let account = deposit
+    let rateAmount = rate * .01  
+    while(account < threshold){
+        account += (account * rateAmount)
+        console.log(account)
+        total++
+    }
+    
+    return total
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170))
--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,35 @@
 export function differentSymbolsNaive(s: string): number {
+    //using an array, but using .includes means bad runtime for long strings
+    // let charArr = s.split('')
+    // let uniqueArr = []
+    // for(let i=0;i < charArr.length;i++){
+    //     //if the character isn't in our unique array
+    //     if(!uniqueArr.includes(charArr[i])){
+    //         //push it in there
+    //         uniqueArr.push(charArr[i])
+    //     }
+    // }
+    // return uniqueArr.length
+
+    //using an object
+    const uniqueChars = {}
+    let charArr = s.split('')
+    charArr.forEach((char) => {
+        //add the character as a property and 1 as its value
+        //by assigning its value as 1 it never adds duplicates. because new cases of the same char return 1 again
+        uniqueChars[char] = 1
+    })
+    //Object.keys is an array of the objects keys. 
+    return Object.keys(uniqueChars).length
+
+    //using a set
+    // const uniqueChars = new Set()
+    // s.split('').forEach((i) => {
+    //     uniqueChars.add(i)
+    // })
+
+    // return uniqueChars.size
 
 }
 
-// console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive('cabca'));

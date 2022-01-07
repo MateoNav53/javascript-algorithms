7cd799427e55ad94e892ad125722676177094451
Mateo Navarrette <mateonav24@gmail.com>
2022-01-07 12:21:22 -0800

    election winners solution

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,14 @@
 export function electionsWinners(votes: number[], k: number): number {
-
+    let contenders = []
+    let highest = Math.max(...votes)
+    votes.forEach((i) => {
+        if((i + k) > highest){
+            contenders.push(i)
+        }
+    })
+    return contenders.length
+    
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([20, 5, 15, 22, 1, 2, 23], 5))

831207d919f13ae9a10cbc33e07154fdbda133b8
Mateo Navarrette <mateonav24@gmail.com>
2022-01-06 21:59:29 -0800

    domain type solution

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,7 @@
 export function domainType(domains: string[]): string[] {
-
+    return domains.map((i) => {
+        return i.includes(".com") ? "commercial" : i.includes(".org") ? "organization" : i.includes(".net") ? "network" : "information"
+    })
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));

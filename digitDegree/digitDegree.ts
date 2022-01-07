c1db5f9f4edc8ddf1ef1235fe12e0d5d77c57334
Mateo Navarrette <mateonav24@gmail.com>
2022-01-06 21:58:45 -0800

    digit degree solution

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,29 @@
-export function digitDegree(n: number): number {
-
+export function digitDegree(n: number) {
+    //show how many times you need to change a number before the sum of its digits total a single digit number
+    let newNum = n
+    let steps = 0
+    if(newNum < 10){
+        return steps
+    }
+    else {
+        while(true){
+            steps++
+            newNum = getDigit(newNum)
+            if(newNum < 10){
+                break
+            }
+        }
+    }
+    return steps
+    
+    
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+function getDigit(num){
+    const nums = num.toString().split('').map((element) => parseInt(element))
+    return nums.reduce((a, b) => a + b)
+}
+
+console.log(digitDegree(5));
+console.log(digitDegree(10));
+console.log(digitDegree(91));

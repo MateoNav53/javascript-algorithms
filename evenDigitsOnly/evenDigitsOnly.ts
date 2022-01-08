b3e5427c6d4842cda8be029d2758e32b2b8f0df3
Mateo Navarrette <mateonav24@gmail.com>
2022-01-07 18:23:03 -0800

    even digits only solution

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,9 @@
 export function evenDigitsOnly(n: number): boolean {
-
+    //check if all digits in a number are even
+    let arr: number[] = n.toString().split('').map((i) => parseInt(i))
+    return arr.every((i) => i % 2 === 0)
+    
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));

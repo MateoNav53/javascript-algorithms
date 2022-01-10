7baddfab0f1fd45db4698587347e170d7f65ed0a
Mateo Navarrette <mateonav24@gmail.com>
2022-01-09 21:36:02 -0800

    fancy ride solution

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,8 @@
 export function fancyRide(l: number, fares: number[]): string {
-
+    let estimates = fares.map((i) => i * l).filter((i) => i <= 20)
+    let rideType = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"]
+    return rideType[estimates.length - 1]
+    
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));

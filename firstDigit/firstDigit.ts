1c5e45577aa9b87eacb73c345eb44e5a04f92008
Mateo Navarrette <mateonav24@gmail.com>
2022-01-10 15:09:01 -0800

    first digit solution

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,8 @@
 export function firstDigit(inputString: string): string {
-
+    let regex = inputString.match(/\d+/)
+    return regex[0]
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'));
+console.log(firstDigit('q2q-q'));
+console.log(firstDigit('0ss'));

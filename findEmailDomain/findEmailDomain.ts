9a747986e6e520529e28d63b882525623c3edfeb
Mateo Navarrette <mateonav24@gmail.com>
2022-01-10 13:24:03 -0800

    find email domain solution

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,7 @@
 export function findEmailDomain(address: string): string {
-
+    let arr = address.split('@')
+    return arr[arr.length - 1]
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));

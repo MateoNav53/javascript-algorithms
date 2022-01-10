a386ff43d17760c3cf116dc97a53329c4aaa1972
Mateo Navarrette <mateonav24@gmail.com>
2022-01-09 16:57:31 -0800

    extract kth solution

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,10 @@
 export function extractEachKth(inputArray: number[], k: number): number[] {
+    //take out the array element at every kth
+    
+    return inputArray.filter((i, index) => {
+        return (index + 1) % k !== 0
+    })
+    
 }
 
-// console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));

3f5c36649cec6d4a658a397348c5900c8a6b111a
Mateo Navarrette <mateonav24@gmail.com>
2022-01-10 12:34:42 -0800

    fermactor solution

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,11 @@
 export function fermactor(n: number): number[] {
-
+    for(let i=0;i < n;i++){
+        for(let j=0;j < i;j++){
+            if(Math.pow(i, 2) - Math.pow(j, 2) === n){
+                return [i, j]
+            }
+        }
+    }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));

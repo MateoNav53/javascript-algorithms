3ee7229cb08f82a9981b1b262a45ce21feff4a22
Mateo Navarrette <mateonav24@gmail.com>
2022-01-09 21:17:22 -0800

    factorialize a number solution

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,18 @@
 export function factorializeANumber(num: number): number {
+    //Using Recursion
+    if(num === 0){
+        return 1
+    } else {
+        return num * factorializeANumber(num - 1)
+    }
 
+    //For loop
+    // let total = 1
+    // for(let i=1;i <= num;i++){
+    //     total *= i
+    // }
+    // return total
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));

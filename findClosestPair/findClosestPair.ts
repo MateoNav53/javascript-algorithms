24a9d23f961c77efd5e63948229947bf65bf4069
Mateo Navarrette <mateonav24@gmail.com>
2022-01-10 12:57:39 -0800

    find closest pair solution

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,6 +1,14 @@
 export function findClosestPair(numbers: number[], sum: number): number {
-
+    let closestDistance = numbers.length
+    for(let i=0;i < numbers.length;i++){
+        for(let j= i + 1;j < numbers.length;j++){
+            if(numbers[i] + numbers[j] === sum && Math.abs(i - j) < closestDistance){
+                closestDistance = Math.abs(i - j)
+            }
+        }
+    }
+    return closestDistance === numbers.length ? -1 : closestDistance
 }
 
-// console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-// console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
+console.log(findClosestPair([2, 3, 7], 8));

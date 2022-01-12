123cc2a3e5c800b25747f28f29b7393b7e6788a0
Mateo Navarrette <mateonav24@gmail.com>
2022-01-12 14:06:07 -0800

    growing plant solution

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,15 @@
 export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    let height = 0
+    let days = 0
+    while(height < desiredHeight){
+        days++
+        height += upSpeed
+        if(height < desiredHeight){
+            height -= downSpeed
+        }
+    }
+    return days
 
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910))

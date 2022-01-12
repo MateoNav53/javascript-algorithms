6e06e6fbe9b9a6bdfe6bb8588ef29ee384a75246
Mateo Navarrette <mateonav24@gmail.com>
2022-01-12 14:04:32 -0800

    flatten array movie

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,25 @@
 export function flattenArray(arr: any[]): any[] {
+    let finalArr = []
+    
+    flatten(arr)
 
+    function flatten(arr){
+        arr.forEach((i) => {
+            if(Array.isArray(i)){
+                console.log(i)
+                //by calling this flatten function i guess we're going deeper into the nested array
+                //so I guess we can call flatten to get deeper into an array
+                flatten(i)
+            }
+            else {
+                finalArr.push(i)
+            }
+        })
+    }
+
+    return finalArr
+    
 }
 
-// console.log(flattenArray([[["a"]], [["b"]]]));
-// console.log(flattenArray([1, [2], [3, [[4]]]]));
+console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([1, [2], [3, [[4]]]]));

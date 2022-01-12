6739a9ca2a77b2d6e5e6143934ffe65d99b5d9d6
Mateo Navarrette <mateonav24@gmail.com>
2022-01-12 14:03:57 -0800

    first not repeating char solution

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,23 @@
 export function firstNotRepeatingCharacter(s: string): string {
+    //Using objects, this might be too much code though
+    let arr = s.split('')
+    let obj = {}
+    for(let val of arr){
+        if(obj.hasOwnProperty(val)){
+            obj[val]++
+        } else {
+            obj[val] = 1
+        }
+    }
+    //return the first object property with the value 1
+    for(const prop in obj){
+        if(obj[prop] === 1){
+            return prop
+        }
+    }
+    return "-"
 
 }
 
-// console.log(firstNotRepeatingCharacter('abacabad'));
-// console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter('abacabad'));
+console.log(firstNotRepeatingCharacter('abacabaabacaba'));

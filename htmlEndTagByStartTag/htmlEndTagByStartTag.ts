e33a7f9b4fe0309be6e9892be67aa8fe3bd20d8f
Mateo Navarrette <mateonav24@gmail.com>
2022-01-14 13:26:17 -0800

    html end tag solution

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,15 @@
 export function htmlEndTagByStartTag(startTag: string): string {
-
+    if(startTag.includes(' ')) {
+        let arr = startTag.split(' ')
+        let newTag = arr[0].split('')
+        newTag.splice(1, 0, '/')
+        return newTag.join('') + '>'
+    } else {
+        let arr = startTag.split('')
+        arr.splice(1, 0, '/')
+        return arr.join('')
+    }
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag('<i>'));

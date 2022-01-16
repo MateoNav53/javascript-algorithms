dc29d2feafdb04696361b4cfd53951f53df842a5
Mateo Navarrette <mateonav24@gmail.com>
2022-01-16 11:43:02 -0800

    incorrect password solution

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,18 @@
 export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+    //for loop with two if statements
+    let attemptCount = 0
+    for(let i=0;i < attempts.length;i++){
+        if(attempts[i] === passcode){
+            attemptCount = 0
+        } else {
+            attemptCount++
+        }
+        if(attemptCount === 10){
+            return true
+        }
+    }
+    return false
 
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));

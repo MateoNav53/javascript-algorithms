02d38e05c45a5c09abd699f46f79d85431a186df
Mateo Navarrette <mateonav24@gmail.com>
2022-01-17 12:37:26 -0800

    isLucky solution

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,6 +1,10 @@
 export function isLucky(n: number): boolean {
-
+    let arr = n.toString().split('')
+    let midIndex = Math.floor(arr.length / 2)
+    let firstHalf = arr.slice(0, midIndex)
+    let secondHalf = arr.slice(midIndex)
+    return firstHalf.reduce((a, b) =>  a + parseInt(b), 0) === secondHalf.reduce((a, b) =>  a + parseInt(b), 0)
 }
 
-// console.log(isLucky(1230));
-// console.log(isLucky(239017));
+console.log(isLucky(1230));
+console.log(isLucky(239017));

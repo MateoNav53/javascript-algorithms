010587d1eb35e5211097633b8177f052933564dd
Mateo Navarrette <mateonav24@gmail.com>
2022-01-17 12:25:35 -0800

    integer to string fixed width solution

--- integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfFixedWidth/integerToStringOfFixedWidth.ts
@@ -1,7 +1,18 @@
 export function integerToStringOfFixedWidth(number: number, width: number): string {
+    let arr = number.toString().split('')
+    if(arr.length < width){
+        while (arr.length < width){
+            arr.unshift('0')
+        }
+    } else if(arr.length > width) {
+        while(arr.length > width) {
+            arr.shift()
+        }
+    }
+    return arr.join('')
 
 }
 
-// console.log(integerToStringOfFixedWidth(1234, 2));
-// console.log(integerToStringOfFixedWidth(1234, 4));
-// console.log(integerToStringOfFixedWidth(1234, 5));
+console.log(integerToStringOfFixedWidth(1234, 2));
+console.log(integerToStringOfFixedWidth(1234, 4));
+console.log(integerToStringOfFixedWidth(1234, 5));

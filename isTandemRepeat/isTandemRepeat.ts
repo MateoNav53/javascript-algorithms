6afd480322c122df216bc7c536460df1a2d13519
Mateo Navarrette <mateonav24@gmail.com>
2022-01-18 14:21:14 -0800

    tandem repeat solution

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,14 @@
 export function isTandemRepeat(inputString: string): boolean {
-
+    if(inputString.length % 2 === 0){
+        let midIndex = Math.floor(inputString.length / 2)
+        let firstHalf = inputString.split('').slice(0, midIndex).join('')
+        let secondHalf = inputString.split('').slice(midIndex).join('')
+        return firstHalf === secondHalf
+    }
+    return false
+    
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))

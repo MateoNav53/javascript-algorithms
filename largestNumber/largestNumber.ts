6eae5aecab4625379dd0e8a036be1cf3406c3b79
Mateo Navarrette <mateonav24@gmail.com>
2022-01-18 17:51:27 -0800

    largest number solution

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,12 @@
 export function largestNumber(n: number): number {
+    // let stringNum = '9'
+    // while(stringNum.length < n){
+    //     stringNum += '9'
+    // }
+    // return parseInt(stringNum)
 
+    // using the .repeat method
+    return parseInt('9'.repeat(n))
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));

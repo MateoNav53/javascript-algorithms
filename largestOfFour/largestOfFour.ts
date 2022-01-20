94c0cbfd5d04e199afbb72ee0af1d8bb051404b6
Mateo Navarrette <mateonav24@gmail.com>
2022-01-20 15:01:21 -0800

    largest of four solution

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,6 +1,8 @@
 export function largestOfFour(nums: number[][]): number[] {
-
+    let arr = []
+    nums.forEach((a) => arr.push(Math.max(...a)))
+    return arr
 }
 
-// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
-// console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));

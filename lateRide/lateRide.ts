5616e4aec0ccb2f512dfe5cc634eaf78e2119303
Mateo Navarrette <mateonav24@gmail.com>
2022-01-21 16:48:14 -0800

    late ride solution

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,11 @@
 export function lateRide(n: number): number {
-
+    let hour = Math.floor(n / 60)
+    console.log(hour)
+    let min = n - (Math.floor(n / 60) * 60)
+    console.log(min)
+    let arr = (hour.toString() + min.toString()).split('')
+    return arr.reduce((a, b) => a + parseInt(b), 0)
 }
 
-// console.log(lateRide(240));
-// console.log(lateRide(808));
+console.log(lateRide(240));
+console.log(lateRide(808));

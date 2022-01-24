3fce7aa27e4bc5b5518ad5c0b50405e7d9647752
Mateo Navarrette <mateonav24@gmail.com>
2022-01-24 14:20:22 -0800

    make array consecutive solution

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,27 @@
 export function makeArrayConsecutive2(statues: number[]): number {
+    //my original solution
+    // let sortedArr = statues.sort((a, b) => a - b)
+    // console.log(sortedArr)
+    // let statuesNeeded = 0
+    // for(let i=0;i < sortedArr.length - 1;i++){
+    //     if(sortedArr[i + 1] !== sortedArr[i] + 1){
+    //         statuesNeeded+= (sortedArr[i + 1] - sortedArr[i] - 1)
+    //     }
+    // }
+    // return statuesNeeded
 
+    //cleaner solution
+    const sortedArr = statues.sort((a, b) => a - b)
+    let statuesNeeded = 0
+    let min = sortedArr[0]
+    let max = sortedArr[sortedArr.length - 1]
+    for(let i=min;i < max;i++){
+        if(!sortedArr.includes(i)){
+            statuesNeeded++
+        }
+    }
+    return statuesNeeded
+    
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 9, 12]));

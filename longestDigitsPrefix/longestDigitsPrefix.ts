0d39e2a4ddd2b6720194e7213a28fef4ba45fc40
Mateo Navarrette <mateonav24@gmail.com>
2022-01-24 14:13:35 -0800

    longest prefix digit solution

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,14 @@
 export function longestDigitsPrefix(inputString: string): string {
-
+    const arr = inputString.split('')
+    let prefix = ''
+    for(let item of arr){
+        const num = parseInt(item)
+        if(isNaN(num)){
+            break
+        }
+        prefix += item
+    }
+    return prefix
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));

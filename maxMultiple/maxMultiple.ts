91ee7976c5ed068c702ae11dbba1609693961441
Mateo Navarrette <mateonav24@gmail.com>
2022-01-26 11:42:11 -0800

    max multiple solution

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,16 @@
 export function maxMultiple(divisor: number, bound: number): number {
+    //for loop
+    // let max = divisor
+    // for(let i=max;i < bound;i++){
+    //     if(i > max && i % divisor === 0){
+    //         max = i
+    //     }
+    // }
+    // return max
 
+    //cleaner solution with modulus
+    let remainder = bound % divisor
+    return bound - remainder
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3,10));

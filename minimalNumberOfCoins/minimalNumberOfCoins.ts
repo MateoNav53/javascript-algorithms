b54c9881ea3e596ba92109388aaccd4730e0a66b
Mateo Navarrette <mateonav24@gmail.com>
2022-01-26 15:54:02 -0800

    minimal number of coins solution

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,18 @@
 export function minimalNumberOfCoins(coins: number[], price: number): number {
+    let arr = []
+    let target = price
+    for(let i=coins.length - 1;i >= 0;i--){
+        while(target > 0){
+            
+        }
+        if(price % coins[i] === 0){
+            arr.push(price / coins[i])
+        } else {
+            arr.push(price / coins[i])
 
+        }
+    }
+    return arr.reduce((a, b) => a + b)
 }
 
-// console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 28));

6649b57c3c002eaebb8ca828a43203745341b7fb
Mateo Navarrette <mateonav24@gmail.com>
2022-01-26 16:07:38 -0800

    missing letters solution

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,8 +1,26 @@
 export function missingLetters(str: string): string {
+    //using char codes
+    for(let i=0;i < str.length - 1;i++){
+        //if the next character isn't one character code greater than the previous
+        if(str.charCodeAt(i + 1) !== str.charCodeAt(i) + 1){
+            //return the missing character
+            return String.fromCharCode(str.charCodeAt(i) + 1)
+        }
+    }
+    return undefined
+    
+    //creating an alphabet array and comparing it to our string. cleaner
+    // let alphabet = "abcdefghijklmnopqrstuvwxyz".split('')
+    // for(let i=0;i < str.length;i++){
+    //     if(str[i] !== alphabet[i]){
+    //         return alphabet[i]
+    //     }
+    // }
+    // return undefined
 
 }
 
-// console.log(missingLetters("bce"));
-// console.log(missingLetters("abce"));
-// console.log(missingLetters("abcdefghjklmno"));
-// console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters("bce"));
+console.log(missingLetters("abce"));
+console.log(missingLetters("abcdefghjklmno"));
+console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));

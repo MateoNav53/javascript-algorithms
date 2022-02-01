600ab2eb54e67aabbea52d7415b15d1933127391
Mateo Navarrette <mateonav24@gmail.com>
2022-02-01 12:38:05 -0800

    new numeral system solution

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,17 @@
 export function newNumeralSystem(number: string): string[] {
+    const arr = []
+    let startChar = 65
+    let endChar = number.charCodeAt(0)
 
+    while(startChar <= endChar){
+        const numeral = `${String.fromCharCode(startChar)} + ${String.fromCharCode(endChar)}`
+        arr.push(numeral)
+        startChar++
+        endChar--
+    }
+
+
+    return arr
 }
 
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('G'));

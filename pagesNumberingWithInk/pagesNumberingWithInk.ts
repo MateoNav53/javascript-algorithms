cf935b2b452488d13c8767460d2aa861a89940f0
Mateo Navarrette <mateonav24@gmail.com>
2022-02-02 12:25:11 -0800

    pages numbering w ink solution

--- pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,20 @@
 export function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
+    let total = 0
+    let fullNumbersArr = []
+    while(total < numberOfDigits){
+        let digitsArr = current.toString().split('')
+        console.log(digitsArr)
+        total += digitsArr.length
+        if(total <= numberOfDigits){
+            fullNumbersArr.push(current)
+        }
+        current++
 
+
+    }
+    return fullNumbersArr[fullNumbersArr.length - 1]
 }
 
-// console.log(pagesNumberingWithInk('G'));
+console.log(pagesNumberingWithInk(1, 5));
+console.log(pagesNumberingWithInk(21, 5));
+console.log(pagesNumberingWithInk(8, 4));

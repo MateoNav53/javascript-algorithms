f536ee9ac5e822b352600672389a7bb6997d18e4
Mateo Navarrette <mateonav24@gmail.com>
2022-02-04 12:41:45 -0800

    pig latin solution

--- pigLatin/pigLatin.ts
+++ pigLatin/pigLatin.ts
@@ -1,6 +1,20 @@
 export function pigLatin(str: string): string {
-
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let stringArr = str.toLowerCase().split('')
+    if(vowels.includes(str.toLowerCase()[0])){
+        return str + 'way'
+    } else {
+        while(true){
+            if(!vowels.includes(stringArr[0])){
+                stringArr.push(stringArr.splice(0, 1)[0])
+            } else {
+                break
+            }
+        }
+        return stringArr.join('') + 'ay'
+        
+    }
 }
 
-// console.log(pigLatin("glove"));
-// console.log(pigLatin("eight"));
+console.log(pigLatin("glove"));
+console.log(pigLatin("eight"));

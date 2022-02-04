7d6a6d5b6058912b2c420f936401a68911fea888
Mateo Navarrette <mateonav24@gmail.com>
2022-02-04 12:30:43 -0800

    solution readme update and palindrome rearranging solution

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,20 @@
 export function palindromeRearranging(inputString: string): boolean {
-
+    const letterObj = {}
+    let oddCount = 0
+    let stringArr = inputString.split('')
+    for(let i=0;i < stringArr.length;i++){
+        if(letterObj.hasOwnProperty(stringArr[i])){
+            letterObj[stringArr[i]]++
+        } else {
+            letterObj[stringArr[i]] = 1
+        }
+    }
+    for(const property in letterObj){
+        if(letterObj[property] % 2 !== 0){
+            oddCount++
+        }
+    }
+    return oddCount <= 1
 }
 
-// console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging('aabb'));

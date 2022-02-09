d13ecd6f6cae2c062bdd16b4ac1bd20132edde72
Mateo Navarrette <mateonav24@gmail.com>
2022-02-09 11:00:36 -0800

    pro categorization solution

--- proCategorization/proCategorization.ts
+++ proCategorization/proCategorization.ts
@@ -1,7 +1,29 @@
 export function proCategorization(pros: string[], preferences: string[][]): string[][][] {
+    const obj = {}
+    const categorizationArr = []
+    for(let i=0;i < preferences.length;i++){
+        for(let j=0;j < preferences[i].length;j++){
+            if(obj[preferences[i][j]]){
+                obj[preferences[i][j]].push(pros[i])
+            } else {
+                obj[preferences[i][j]] = [pros[i]]
+            }
+        }
+    }
+    for(const property in obj){
+        categorizationArr.push([[property], obj[property]])
+    }
 
+
+    return categorizationArr.sort((a, b) => {
+        if(a[0][0] > b[0][0]){
+            return 1
+        } else {
+            return -1
+        }
+    })
 }
 
-// console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
-// ["Computer lessons", "Computer repair", "Data recovery service"],
-// ["Computer lessons", "House cleaning"]]));
+console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
+["Computer lessons", "Computer repair", "Data recovery service"],
+["Computer lessons", "House cleaning"]]));

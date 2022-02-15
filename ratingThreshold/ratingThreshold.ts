9122a0bd034c3c13713b216f6cf588144ba85287
Mateo Navarrette <mateonav24@gmail.com>
2022-02-15 12:26:15 -0800

    ratings threshold solution

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,15 @@
 export function ratingThreshold(threshold: number, ratings: number[][]): number[] {
-
+    let needsReview = []
+    for(let i=0;i < ratings.length;i++){
+        let avg = ratings[i].reduce((a, b) => a + b, 0) / ratings[i].length
+        if(avg < threshold){
+            needsReview.push(i)
+        }
+    }
+    return needsReview
 }
 
-// console.log(ratingThreshold(3.5, 
-//     [[3, 4],
-//     [3, 3, 3, 4],
-//     [4]]));
+console.log(ratingThreshold(3.5, 
+    [[3, 4],
+    [3, 3, 3, 4],
+    [4]]));

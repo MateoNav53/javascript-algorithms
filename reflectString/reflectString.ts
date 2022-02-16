72b6bb0023ba5e3e50768e42f1146ad32c123dca
Mateo Navarrette <mateonav24@gmail.com>
2022-02-16 14:32:23 -0800

    reflect string solution

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,14 @@
 export function reflectString(inputString: string): string {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const inputArr = inputString.split('')
+    let reflectedString = ''
+    for(let i=0;i < inputArr.length;i++){
+        let reflectedIndex = Math.abs(alphabet.indexOf(inputArr[i]) - 25)
+        reflectedString += alphabet[reflectedIndex]
+    }
+
+    return reflectedString
 
 }
 
-// console.log(reflectString("name"));
+console.log(reflectString("name"));
